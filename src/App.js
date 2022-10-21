b49d96a72f1fefa66d3397794fb18dc6132e59b3
SebastianDeFranco <sebastianedefranco@gmail.com>
2022-10-20 21:08:23 -0300

    probando

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <h1>Probando</h1>
     </div>
   );
 }
